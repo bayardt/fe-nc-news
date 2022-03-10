db1c9b4bef07c14faf173a26f7d22cffee7f5bd8
Bayard <bayardtarpley@gmail.com>
2022-03-10 10:50:41 +0000

    updating with topics filter

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-export default function ArticleCard({article}) {
+export default function ArticleCard({ article }) {
   return (
     <div className="m-articleCard">
       <h4 className="a-articleCard__topic">{article.topic}</h4>
       <h3 className="a-articleCard__title">{article.title}</h3>
-      <article className='a-articleCard__article'>{article.body}</article>
+      <article className="a-articleCard__article">{article.body}</article>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,30 +4,33 @@ import NCNewsLogo from "../images/NCNewsLogo.png";
 import * as api from "../api";
 
 export default function Header() {
-  const [topics, setTopics] = useState([""]);
+  const [topics, setTopics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    api.getTopics().then(({ topics }) => setTopics(topics));
+    setIsLoading(true);
+    api.getTopics().then(({ topics }) => {
+      setTopics(topics);
+      setIsLoading(false);
+    });
   }, []);
 
+  if (isLoading) return <p>Loading...</p>;
   return (
     <div className="o-header">
       <div className="m-header__Logo">
         <img src={NCNewsLogo} alt="Northcoders News" />
       </div>
       <nav className="m-topicsNav">
-        <Link to={"/"}>| home |</Link> 
-          {topics.map((topic) => {
-            const topicPath = `/topic/${topic.slug}`
-            return (
-              <Link
-                to={topicPath}
-                key={topic.slug}
-              >
-                | {topic.slug} |
-              </Link>
-            );
-          })}
+        <Link to={"/"}>| home |</Link>
+        {topics.map((topic) => {
+          const topicPath = `/topic/${topic.slug}`;
+          return (
+            <Link to={topicPath} key={topic.slug}>
+              | {topic.slug} |
+            </Link>
+          );
+        })}
       </nav>
     </div>
   );
