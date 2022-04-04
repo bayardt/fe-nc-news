db22da98c13cecb7afca737e165b47066b1de637
Bayard <bayardtarpley@gmail.com>
2022-04-04 11:48:24 +0100

    add indicator of logged in user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import NCNewsLogo from "../images/NCNewsLogo.png";
 import * as api from "../api";
+import { UserContext } from "../contexts/UserStatus";
 
 export default function Header() {
   const [topics, setTopics] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const { currentUser } = useContext(UserContext);
 
   useEffect(() => {
     setIsLoading(true);
@@ -22,15 +24,17 @@ export default function Header() {
         <img src={NCNewsLogo} alt="Northcoders News" />
       </div>
       <nav className="m-topicsNav">
-        <Link to={"/"}>| home |</Link> 
-          {topics.map((topic) => {
-            return (
-              <Link to={`/topic/${topic.slug}`} key={topic.slug}>
-                | {topic.slug} |
-              </Link>
-            );
-          })}
+        <Link to={"/"}>| home |</Link>
+        {topics.map((topic) => {
+          return (
+            <Link to={`/topic/${topic.slug}`} key={topic.slug}>
+              | {topic.slug} |
+            </Link>
+          );
+        })}
       </nav>
+      <small>Logged in: {currentUser}</small>
+      <br />
       <br />
     </div>
   );
